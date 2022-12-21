3d8c0b6ca05710eee340a9f8ea82ab08f3b7209c
Noel_Niko <noel.nosse@grainger.com>
2022-12-21 15:50:17 -0500

    Development-101 # List items now draggable

--- app/src/main/java/com/livingtechusa/reflexion/ui/customLists/CustomListContent.kt
+++ app/src/main/java/com/livingtechusa/reflexion/ui/customLists/CustomListContent.kt
@@ -1,15 +1,18 @@
 package com.livingtechusa.reflexion.ui.customLists
 
+import android.util.Log
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.draggable
+import androidx.compose.foundation.gestures.rememberDraggableState
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
@@ -19,14 +22,18 @@ import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
-import com.livingtechusa.reflexion.ui.build.BuildEvent
 import com.livingtechusa.reflexion.ui.viewModels.CustomListsViewModel
-import com.livingtechusa.reflexion.util.ReflexionArrayItem
+import kotlin.math.roundToInt
 
 @Composable
 fun CustomListContent(
@@ -35,12 +42,14 @@ fun CustomListContent(
 ) {
     val customList by viewModel.customList.collectAsState()
     val listOfLists by viewModel.listOfLists.collectAsState()
-
+    val context = LocalContext.current
     LazyColumn(
-        modifier = Modifier.fillMaxSize().padding(4.dp),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(4.dp),
         verticalArrangement = Arrangement.spacedBy(4.dp),
     ) {
-        items( 1) { index -> //customList.items?.size ?:
+        items(1) { index -> //customList.items?.size ?:
             Column(modifier = Modifier.background(Color.LightGray)) {
                 TextField(
                     value = customList.reflexionItemName.toString(),
@@ -50,7 +59,12 @@ fun CustomListContent(
                         backgroundColor = Color.Transparent
                     ),
                     onValueChange = {
-                        viewModel.onTriggerEvent(CustomListEvent.UpdateListName(index = index, text = it))
+                        viewModel.onTriggerEvent(
+                            CustomListEvent.UpdateListName(
+                                index = index,
+                                text = it
+                            )
+                        )
                     }
                 )
                 Spacer(
@@ -59,14 +73,26 @@ fun CustomListContent(
                         .fillMaxWidth()
                 )
                 LazyRow(
-                    modifier = Modifier.fillMaxHeight().padding(20.dp),
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .padding(20.dp),
                     horizontalArrangement = Arrangement.spacedBy(4.dp),
                 ) {
                     if (customList.items.isNullOrEmpty().not()) {
                         val count: Int = customList.items?.size ?: 0
-                        items(count) {
+                        items(count) { item ->
+                            var offsetX by remember { mutableStateOf(0f) }
+
                             Text(
-                                text = customList.items?.get(it)?.reflexionItemName.toString() + ", "
+                                modifier = Modifier
+                                    .offset { IntOffset(offsetX.roundToInt(), 0) }
+                                    .draggable(
+                                        orientation = Orientation.Horizontal,
+                                        state = rememberDraggableState { delta ->
+                                            offsetX += delta
+                                        }
+                                    ),
+                                text = customList.items?.get(item)?.reflexionItemName.toString() + ", "
                             )
                         }
                     }
